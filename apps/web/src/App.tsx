7b318d662e4667a6210be683903bff09490dd6ad
김민지 <twoji3019@naver.com>
2025-06-16 14:34:33 +0900

    Test : fetch에서 가져온 데이터로 화면 출력 성공

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,8 +2,18 @@ import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState('')
+
+  // fetch로 가져오기
+  useEffect(()=>{
+    fetch('http://localhost:3001/hello')
+    .then(res=>res.json())
+    .then(data=>setMessage(data.message))
+    .catch(err=> console.log('데이터 불러오기 실패',err))
+  },[])
 
   return (
+    <h1>{message || '로딩중...'}</h1>
   )
 }
 
