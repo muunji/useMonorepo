3e84fb6215247b51c52b21464c585c6d894add55
김민지 <twoji3019@naver.com>
2025-06-18 17:42:13 +0900

    Build : [elec] renderer 빌드를 위한 패키지 설치 및 파일 작성

---
+++ monorepo/apps/electron/vite.renderer.config.ts
@@ -0,0 +1,18 @@
+// vite 설정 파일
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  root:'./src/renderer',
+  build:{
+    outDir:'../../dist/apps/electron/renderer',
+    emptyOutDir:true,
+  },
+  plugins:[react()],
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'./src/renderer')
+    }
+  }
+})
