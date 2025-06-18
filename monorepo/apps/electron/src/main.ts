31731f30f6cc501c5b72ef2e4c1d534e714c5706
김민지 <twoji3019@naver.com>
2025-06-18 17:27:32 +0900

    Test : [elec] React 세팅 후 메인 프로세스 빌드

--- monorepo/apps/electron/src/main.ts
+++ monorepo/apps/electron/src/main.ts
@@ -1,1 +1,17 @@
 console.log('Hello World');
+// React 렌더러 로드
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path';
+
+function createWindow(){
+  const win = new BrowserWindow({
+    width:800,
+    height:600,
+    webPreferences:{nodeIntegration:true, contextIsolation:false},
+  })
+
+  // React가 빌드외어 있는 파일 경로 지정
+  win.loadFile(path.join(__dirname,'renderer','index.html'))
+}
+
+app.whenReady().then(createWindow)
---
+++ monorepo/apps/electron/src/renderer/App.tsx
@@ -0,0 +1,6 @@
+//컴포넌트 작성
+import React from 'react';
+
+export function App(){
+  return <h1>Hello World</h1>
+}
