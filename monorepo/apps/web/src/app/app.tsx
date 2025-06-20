ff9e9c1074f9a8f2d94779b17b12ac9e2d80e7a5
김민지 <twoji3019@naver.com>
2025-06-20 10:46:13 +0900

    Test : [web] api에서 text 가져옴

--- monorepo/apps/web/src/app/app.tsx
+++ monorepo/apps/web/src/app/app.tsx
@@ -1,9 +1,20 @@
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
+import { useState, useEffect } from "react";
 
 export function App() {
+  const [text, setText] = useState('')
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/hello')
+    .then(res=>res.json())
+    .then(data=> setText(data.text))
+    .catch(err=>console.error('데이터 오류',err))
+  },[])
+
   return (
     <div>
-      <NxWelcome title="web" />
+      {/* <NxWelcome title="web" /> */}
+      <h1>{text}</h1>
     </div>
   );
 }
