28d4d709bbaf38aa698e2e79e2c46854c4b5cda0
김민지 <twoji3019@naver.com>
2025-06-20 11:07:40 +0900

    Test : [elec] 호출 완료

--- monorepo/apps/electron/src/renderer/App.tsx
+++ monorepo/apps/electron/src/renderer/App.tsx
@@ -1,6 +1,15 @@
 //컴포넌트 작성
-import React from 'react';
+import { useEffect, useState } from "react"
 
 export function App(){
-  return <h1>Hello World</h1>
+  const [text, setText]=useState('')
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/hello')
+    .then(res=>res.json())
+    .then(data=> setText(data.text))
+    .catch(err=>console.error('데이터 오류',err))
+  },[])
+
+  return <h1>{text}</h1>
 }
