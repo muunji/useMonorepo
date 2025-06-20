f5ba254bb53475b84c518e5271023d81e0c3d485
김민지 <twoji3019@naver.com>
2025-06-20 15:52:52 +0900

    Chore : 공통 컴포넌트 - web, react-native 둘다 사용 가능하도록 작성

--- monorepo/libs/shared/ui/src/lib/TestBtn.tsx
+++ monorepo/libs/shared/ui/src/lib/TestBtn.tsx
@@ -1,5 +1,10 @@
-export const TestBtn = ({onClick}:{onClick:()=>void})=>{
+import { Text, Pressable } from 'react-native'
+// * click 이벤트를 가진 버튼 역할 -> <Pressable> + onPress 사용
+
+export const TestBtn = ({onPress}:{onPress:()=>void})=>{
 return (
-  <button onClick={onClick}>테스트</button>
+  <Pressable onPress={onPress}>
+    <Text>테스트</Text>
+  </Pressable>
 )
 }
