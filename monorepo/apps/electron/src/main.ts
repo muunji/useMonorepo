af3f9ec722fd467888680ec3fdda2201d0769f7f
김민지 <twoji3019@naver.com>
2025-06-20 14:45:03 +0900

    Feat : [web] [elec] 화면 동기화 및 scripts에 한 번에 실행되도록 추가

--- monorepo/apps/electron/src/main.ts
+++ monorepo/apps/electron/src/main.ts
@@ -1,17 +1,18 @@
 console.log('Hello World');
 // React 렌더러 로드
 import { app, BrowserWindow } from 'electron'
-import * as path from 'path';
 
 function createWindow(){
   const win = new BrowserWindow({
     width:800,
     height:600,
-    webPreferences:{nodeIntegration:true, contextIsolation:false},
+    webPreferences:{
+      nodeIntegration:false, //보안상 
+      contextIsolation:false},
   })
 
   // React가 빌드되어 있는 파일 경로 지정
-  win.loadFile(path.join(__dirname,'renderer','index.html'))
+  win.loadURL('http://localhost:4200')
 }
 // console.log(__dirname)
 app.whenReady().then(createWindow)
