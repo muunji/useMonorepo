faa547487a198fd8ca74c64eca2ab3995d57a089
김민지 <twoji3019@naver.com>
2025-06-25 09:50:56 +0900

    Fix : [mobile] 엔트리 포인트에 맞춰 export로 수정

--- monorepo/apps/mobile/src/main.tsx
+++ monorepo/apps/mobile/src/main.tsx
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native';
+// import { AppRegistry } from 'react-native';
 import App from './app/App';
 
-AppRegistry.registerComponent('Mobile', () => App);
+//index.js (엔트리)로 권한 위임
+// AppRegistry.registerComponent('Mobile', () => App);
+
+export default App;
